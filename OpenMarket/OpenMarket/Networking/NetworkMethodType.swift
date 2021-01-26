9e17846d70da19e728f0230882aa0dae7009ef40
wonhee <wonhee010@gmail.com>
2021-01-26 12:13:34 +0900

    feat: add Netsork Method Type enum class

---
+++ OpenMarket/OpenMarket/Networking/NetworkMethodType.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkMethodType.swift
+//  OpenMarket
+//
+//  Created by Wonhee on 2021/01/26.
+//
+
+import Foundation
+
+enum NetworkMethodType: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
