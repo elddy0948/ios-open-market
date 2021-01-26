e27498c80eb7dc85cead8b07da57f02f1149dcf4
wonhee <wonhee010@gmail.com>
2021-01-26 12:11:50 +0900

    feat: add GoodsItem type

--- OpenMarket/OpenMarket/Model/Data/GoodsItem.swift
+++ OpenMarket/OpenMarket/Model/Data/GoodsItem.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
-struct GoodsItem {
+struct GoodsItem: Decodable {
+    let id: Int
+    let title: String
+    let price: Int
+    let currency: String
+    let stock: Int
+    let thumbnails: [String]
+    let descriptions: String?
+    let discountedPrice: Int?
+    let registrationDate: TimeInterval
     
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case price
+        case currency
+        case stock
+        case thumbnails
+        case descriptions
+        case discountedPrice = "discounted_price"
+        case registrationDate = "registration_date"
+    }
 }
