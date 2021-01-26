ab41c0b941ca92f821377da1e6de79dba8e42039
wonhee <wonhee010@gmail.com>
2021-01-26 16:39:16 +0900

    feat: add network error case

--- OpenMarket/OpenMarket/Error/NetworkError.swift
+++ OpenMarket/OpenMarket/Error/NetworkError.swift
@@ -10,6 +10,7 @@ import Foundation
 enum NetworkError: Error {
     case network
     case getData
+    case convertStringToURL
 }
 
 extension NetworkError: LocalizedError {
@@ -19,6 +20,8 @@ extension NetworkError: LocalizedError {
             return "네트워크 통신 중 문제가 발생했습니다.\n잠시 후 다시 시도해주세요."
         case .getData:
             return "데이터를 가져오는 중 문제가 발생했습니다.\n잠시 후 다시 시도해주세요."
+        case .convertStringToURL:
+            return "URL을 가져오는 중 문제가 발생했습니다.\n잠시 후 다시 시도해주세요."
         }
     }
 }
---
+++ OpenMarket/OpenMarket/Networking/OpenMarketAPITypes.swift
@@ -0,0 +1,8 @@
+//
+//  OpenMarketAPITypes.swift
+//  OpenMarket
+//
+//  Created by Wonhee on 2021/01/26.
+//
+
+import Foundation
