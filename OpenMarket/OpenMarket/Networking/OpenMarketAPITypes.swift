f35d853d89322b5594769d1d870eb8f4fc3c0a53
wonhee <wonhee010@gmail.com>
2021-01-26 16:39:39 +0900

    feat: add query struct

--- OpenMarket/OpenMarket/Networking/OpenMarketAPITypes.swift
+++ OpenMarket/OpenMarket/Networking/OpenMarketAPITypes.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+enum OpenMarketAPITypes{
+    case getGoodsList(page: Int)
+    case registerGoods
+    case getGoods(id: Int)
+    case editGoods(id: Int)
+    case deleteGoods(id: Int)
+    
+    var urlQuery: String {
+        switch self {
+        case .getGoodsList(page: let page):
+            return "/items/\(page)"
+        case .registerGoods:
+            return "/item"
+        case .getGoods(id: let id):
+            return "/item/\(id)"
+        case .editGoods(id: let id):
+            return "/item/\(id)"
+        case .deleteGoods(id: let id):
+            return "/item/\(id)"
+        }
+    }
+}
