ba14bfca421e3ce3bb6faf5e962d154325344710
wonhee <wonhee010@gmail.com>
2021-01-26 16:39:29 +0900

    feat: add makeurl func

--- OpenMarket/OpenMarket/Networking/NetworkConfig.swift
+++ OpenMarket/OpenMarket/Networking/NetworkConfig.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 struct NetworkConfig {
-    private static let openMarketFixedURL = "https://camp-open-market.herokuapp.com/"
+    private static let openMarketFixedURL = "https://camp-open-market.herokuapp.com"
+    
+    static func makeURL(with api: OpenMarketAPITypes) -> URL? {
+        var urlString = openMarketFixedURL
+        urlString.append(api.urlQuery)
+        return URL(string: urlString)
+    }
 }
